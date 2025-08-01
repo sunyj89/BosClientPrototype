075343159bb899f54acd67daf734ad5d0cfa399c
shenbo <shenbo@weicheche.cn>
2025-08-01 17:29:21 +0800

    refactor(proxy): 重构代理配置并添加dayjs依赖
    将代理路径配置从package.json移至setupProxy.js，使用pathFilter替代
    添加dayjs日期处理库依赖

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -8,11 +8,11 @@ module.exports = function(app) {
     console.log('✅ setupProxy.js 已加载-------'); // 关键验证点
 
     app.use(
-        '/merchant',
         createProxyMiddleware({
             target: 'http://jxgs-newos-merchantmanage.zhihuiyouzhan.com:81',
             changeOrigin: true,
-            logLevel: 'debug'
+            logLevel: 'debug',
+            pathFilter:'/merchant',
         })
     );
 
